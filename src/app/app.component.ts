8c14b05715c7f8ca88e8d7a428f6007ef0e24758
Richard <rich10tom@gmail.com>
2021-10-21 20:31:08 -0500

    Exercise 1 for the template driven approach to forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BasicTemplateDrivenExercise';
+
+  @ViewChild('f', {static: false}) formInput!: NgForm;
+
+  onSubmit(formInput: NgForm) {
+    console.log(formInput.value)
+  }
+
 }
